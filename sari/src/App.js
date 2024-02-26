53323f06018fdea3cf4d2f2e99ac8a3b06b69c0c
ramazantuf <ramazan@radyobilge.com>
2024-02-26 12:50:49 +0000

    env api url

--- sari/src/App.js
+++ sari/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>{process.env.REACT_APP_API_URL}</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
