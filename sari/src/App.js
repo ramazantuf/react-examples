6cf8837ae76506f099085949f30236fbd7047db1
ramazantuf <ramazan@radyobilge.com>
2024-02-27 07:51:55 +0000

    useRef ForwardRef

--- sari/src/App.js
+++ sari/src/App.js
@@ -4,10 +4,21 @@ import './index.css';
 import Button from './components/Button';
 import Tab from './components/Tab';
 import Test from './components/Test';
-import { useState } from 'react';
+import { useState,useRef,forwardRef } from 'react';
+{/**forward Ref */}
+function Input(props,ref){
+  return <input ref={ref} type="text" {...props}/>
+}
+
+Input = forwardRef(Input)
 
 function App() {
   const [show, setShow] = useState(false)
+  const inputRef = useRef()
+
+  const focusInput = ()=>{
+    inputRef.current.focus()
+  }
   return (
     <>
       <h1>Ramazan T</h1>
@@ -23,6 +34,10 @@ function App() {
           <Tab.Panel title="Ayarlar">3. Tab</Tab.Panel>
         </Tab>
         </div>
+        {/* useRef */}
+        <input type="text" ref={inputRef}/>
+        <Input ref={inputRef}/>
+        <button onClick={focusInput}>Focusla</button>
       <Button text="Buton Örneği" variant="success"/>
     </>
   );
