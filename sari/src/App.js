c772fad60291b22c3e84fd62a25eca1716cc1389
ramazantuf <ramazan@radyobilge.com>
2024-02-27 08:20:42 +0000

    usereducer

--- sari/src/App.js
+++ sari/src/App.js
@@ -1,44 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 import './index.css';
-import Button from './components/Button';
-import Tab from './components/Tab';
-import Test from './components/Test';
-import { useState,useRef,forwardRef } from 'react';
-{/**forward Ref */}
-function Input(props,ref){
-  return <input ref={ref} type="text" {...props}/>
+import { useState,useReducer } from 'react';
+
+function reducer(state,action){
+  console.log(state,action)
+  switch(action.type){
+    case 'SET_TODO':
+      return{
+        ...state,
+        todo: action.value
+      }
+    case 'ADD_TODO':
+      return{
+        ...state,
+        todo:'',
+        todos: [
+          ...state.todos,
+          action.todo
+        ]
+      }
+  }
+
 }
 
-Input = forwardRef(Input)
-
 function App() {
-  const [show, setShow] = useState(false)
-  const inputRef = useRef()
+  const [state,dispatch] = useReducer(reducer,{
+    todos: [],
+    todo: ''
 
-  const focusInput = ()=>{
-    inputRef.current.focus()
-  }
+  });
+ const [todos, setTodos] = useState([])
+ const [todo, setTodo] = useState()
+
+ const submitHandle = e=>{
+  e.preventDefault()
+  //setTodos([...todos, todo])
+ // setTodo('')
+ dispatch({
+  type:'ADD_TODO',
+  todo: state.todo
+ })
+ }
+
+ const onChange = e=>{
+  dispatch({
+    type: 'SET_TODO',
+    value: e.target.value
+  })
+ }
+
   return (
     <>
-      <h1>Ramazan T</h1>
-      <>
-      <button onClick={()=>setShow(show=>!show)}>{show ? 'Gizle':'Göster'}</button>
-        {show && <Test/>}
-      </>
-      <div style={{padding:20}}>
-        
-        <Tab activeTab={1}>
-          <Tab.Panel title="Profil">1. Tab</Tab.Panel>
-          <Tab.Panel title="Hakkında">2. Tab</Tab.Panel>
-          <Tab.Panel title="Ayarlar">3. Tab</Tab.Panel>
-        </Tab>
-        </div>
-        {/* useRef */}
-        <input type="text" ref={inputRef}/>
-        <Input ref={inputRef}/>
-        <button onClick={focusInput}>Focusla</button>
-      <Button text="Buton Örneği" variant="success"/>
+      <h1>Todo App</h1>
+      <form onSubmit={submitHandle}>
+        <input type='text' value={state.todo} onChange={onChange}/>
+        <button disabled={!state.todo} type='submit'>Ekle</button>
+      </form>
+      <ul>
+        {state.todos.map((todo,index)=>(
+          <li key={index}>
+            {todo}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
