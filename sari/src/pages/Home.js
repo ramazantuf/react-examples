3890eecce5c3c34ee4e5d5879f813bb2fd7e1cec
ramazantuf <ramazan@radyobilge.com>
2024-02-29 05:31:17 +0000

    helmet seo

--- sari/src/pages/Home.js
+++ sari/src/pages/Home.js
@@ -1,6 +1,11 @@
+import {Helmet} from "react-helmet";
 export default function Home(){
     return (
         <div>
+            <Helmet>
+                <title>Anasayfa</title>
+                <meta name="description" content="anasayfa description"/>
+            </Helmet>
             Home Page
         </div>
     )
